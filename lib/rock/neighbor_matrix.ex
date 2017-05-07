4754429e0d2986e45a8be7613e59ed6804d39332
Ayrat Badykov <ayratin555@gmail.com>
2017-05-07 15:34:33 +0300

    optimize calculation of neighbor matrix by calculating only lower triangular matrix

--- lib/rock/neighbor_matrix.ex
+++ lib/rock/neighbor_matrix.ex
@@ -2,14 +2,63 @@ defmodule Rock.NeighborMatrix do
   alias Rock.JaccardCoefficient
 
   def matrix(points,
+      theta,
       similarity_function \\ &JaccardCoefficient.measure/2)
       when is_list(points) do
+    similarity_function = fn(point1, point2) ->
+      neighbors?(point1, point2, similarity_function, theta)
+    end
+
     points
-    |> Enum.map(fn(point1) ->
-      points
-      |> Enum.map(fn(point2) ->
+    |> lower_triangle_matrix(similarity_function)
+    |> copy_to_upper_triangle
+  end
+
+  defp lower_triangle_matrix(points, similarity_function) do
+    points
+    |> Enum.with_index
+    |> Enum.map(fn({point1, row_index}) ->
+      lower_triangle_row(point1, row_index, points, similarity_function)
+    end)
+  end
+
+  defp lower_triangle_row(point1, row_index, points, similarity_function) do
+    points
+    |> Enum.with_index
+    |> Enum.map(fn({point2, column_index}) ->
+      if row_index >= column_index do
         similarity_function.(point1, point2)
+      else
+        0
+      end
+    end)
+  end
+
+  defp neighbors?(point1, point2, similarity_function, theta) do
+    if similarity_function.(point1, point2) > theta, do: 1, else: 0
+  end
+
+  defp copy_to_upper_triangle(lower_neighbor_matrix) do
+    lower_neighbor_matrix
+    |> Enum.with_index
+    |> Enum.map(fn({row, row_index}) ->
+      row
+      |> Enum.with_index
+      |> Enum.map(fn({element, column_index}) ->
+        lower_neighbor_matrix
+        |> lower_triangle_element(row_index, column_index, element)
       end)
     end)
+  end
+
+  defp lower_triangle_element(matrix, row_index, column_index, _element)
+      when row_index < column_index do
+    matrix
+    |> Enum.at(column_index)
+    |> Enum.at(row_index)
+  end
+
+  defp lower_triangle_element(_matrix, _row_index, _column_index, element) do
+    element
   end
 end
