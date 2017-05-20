a4731aa60a7f66e68bc61feffe484785c97d0e19
Ayrat Badykov <ayratin555@gmail.com>
2017-05-20 22:07:49 +0300

    add test for heap creation

--- test/rock/struct/heap_test.exs
+++ test/rock/struct/heap_test.exs
@@ -7,20 +7,20 @@ defmodule Rock.Struct.HeapTest do
 
   test "initializs heap" do
     points = [
-      Point.new(["1", "2", "3"]),
-      Point.new(["1", "2", "4"]),
-      Point.new(["1", "2", "5"]),
-      Point.new(["1", "3", "4"]),
-      Point.new(["1", "3", "5"]),
-      Point.new(["1", "4", "5"]),
-      Point.new(["2", "3", "4"]),
-      Point.new(["2", "3", "5"]),
-      Point.new(["2", "4", "5"]),
-      Point.new(["3", "4", "5"]),
-      Point.new(["1", "2", "6"]),
-      Point.new(["1", "2", "7"]),
-      Point.new(["1", "6", "7"]),
-      Point.new(["2", "6", "7"])
+      Point.new("1", ["1", "2", "3"], 0),
+      Point.new("2", ["1", "2", "4"], 1),
+      Point.new("3", ["1", "2", "5"], 2),
+      Point.new("4", ["1", "3", "4"], 3),
+      Point.new("5", ["1", "3", "5"], 4),
+      Point.new("6", ["1", "4", "5"], 5),
+      Point.new("7", ["2", "3", "4"], 6),
+      Point.new("8", ["2", "3", "5"], 7),
+      Point.new("9", ["2", "4", "5"], 8),
+      Point.new("10", ["3", "4", "5"], 9),
+      Point.new("11", ["1", "2", "6"], 10),
+      Point.new("12", ["1", "2", "7"], 11),
+      Point.new("13", ["1", "6", "7"], 12),
+      Point.new("14", ["2", "6", "7"], 13)
     ]
     link_matrix = [
       [0, 7, 7, 5, 5, 4, 5, 5, 4, 4, 5, 5, 2, 2],
@@ -46,8 +46,19 @@ defmodule Rock.Struct.HeapTest do
     clusters =
       point_clusters
       |> Enum.map(&Cluster.new(&1))
-    cluster = clusters |>  Enum.at(0)
+    cluster = %Cluster{uuid: cluster_uuid} =  clusters |>  Enum.at(0)
     clusters = clusters |> List.delete_at(0)
+
+    heap = cluster |> Heap.new(clusters, link_matrix, theta)
+
+    %Heap{cluster_uuid: ^cluster_uuid, items: items} = heap
+    clusters
+    |> Enum.each(fn(%Cluster{uuid: uuid}) ->
+      items
+      |> Enum.any?(fn({_, _, item_uuid}) ->
+        item_uuid == uuid
+      end)
+    end)
   end
 end
  
